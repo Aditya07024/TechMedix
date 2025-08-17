73cd5ba21d4647b65a819dfa6b54b8d376c8d484
Aditya07024 <adityakumar07024@gmail.com>
2025-08-18 01:57:46 +0530

    Fix axios import issue for Vite build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [], // ensure axios isn't accidentally externalized
-    },
-  },
   optimizeDeps: {
-    include: ["axios"], // force Vite to bundle axios
+    include: ["axios"], // ensure axios is bundled
   },
 })
